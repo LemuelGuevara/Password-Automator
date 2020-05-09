72aa09831a86298663ac22326370bc75f780d02e
RiderxL <lemuelkuya@gmail.com>
2020-05-10 00:45:33 +0800

    Updated

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 import random
 import string
+import sys
 
-def auto():
-	keygen = (string.ascii_letters + string.digits)
-	randomator = [("".join(random.choice(keygen))) for i in range(8)]
-	return ("".join(randomator))
 
-print(auto())
+class Automator:
+    def makeUsername(self):
+        userList = open('list.txt', 'r').readlines()
+        i =+ 1
+        return (random.choice(userList))
+
+    def makePass(self):
+        keygen = (string.ascii_letters + string.digits)
+        randomPass = [("".join(random.choice(keygen) for i in range(8)))]
+        return ("".join(randomPass))
+    
+    def output(self):
+        username = self.makeUsername()
+        password = self.makePass()
+
+        while True:
+            print(f"\nUsername: {username}Password: {password}")
+            break
+        
+auto = Automator()
+auto.output()
